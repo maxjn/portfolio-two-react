634bd5beb1520964febb8d5fdb02272808237f3a
max jn <maxjn763@gmail.com>
2023-04-24 11:14:51 +0430

    Initial Release: - adding defualt structure - adding NavBar

---
+++ README.md
@@ -0,0 +1,33 @@
+# Portfolio -React
+
+This is a simple portfolio application for practicing React & Tailwind.
+
+See [Live Project](https://maxjn-portfolio-two.pages.dev/).
+This Project was bootstrapped with [Vite](https://vitejs.dev/).
+
+## Technologies
+
+- HTML,Tailwind
+- ES6
+- React
+- **react-icons** package for icons
+- **ract-smooth-scroll**
+
+## Features
+
+- Mobile Firs
+- Responsive
+
+## Installation
+
+Install **Portfolio-two** with npm
+
+```shell
+npm install
+
+npm run dev
+```
+
+## Screenshots
+
+![Cover](./public/cover.png)
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function NavBar() {
+  const [nav, setNav] = useState(false);
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experience",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+  return (
+    <header
+      className="flex items-center justify-between bg-black
+     text-white px-5 py-3 fixed w-full h-[4.5rem]"
+    >
+      {/* Hero Text */}
+      <h1 className="text-5xl font-bold font-signiture">maxjn</h1>
+      {/* Nav Menu */}
+      <nav>
+        {/* Desktop Menu */}
+        <ul className="md:flex gap-6 capitalize text-gray-400 font-medium hidden">
+          {links.map(({ link, id }) => (
+            <li key={id} className="hover:scale-110 duration-75 cursor-pointer">
+              {link}
+            </li>
+          ))}
+        </ul>
+        {/* OffCanvas Menu */}
+        {nav && (
+          <ul className="absolute flex flex-col text-5xl items-center justify-center gap-10 capitalize text-gray-500 bg-gradient-to-b from-black to-gray-800 font-medium  left-0 right-0 top-[4.5rem] h-screen px-60 md:hidden ">
+            {links.map(({ link, id }) => (
+              <li
+                key={id}
+                className="hover:scale-110 duration-75 cursor-pointer"
+              >
+                {link}
+              </li>
+            ))}
+          </ul>
+        )}
+      </nav>
+      {/* OffCanvas Button */}
+      <div
+        className="text-gray-500 md:hidden cursor-pointer"
+        onClick={() => setNav(!nav)}
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
