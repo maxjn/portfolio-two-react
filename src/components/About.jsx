50aaeb4fa6d8a09f221f628dec10444d99edf059
Muhammad Haghnegahdar <104886566+maxjn@users.noreply.github.com>
2023-04-24 15:02:01 +0330

    Components: (#1)
    - added Home,About,SocialLinks components

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+function About() {
+  return (
+    <section
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800 text-white flex items-center"
+    >
+      <div className="max-w-screen-lg  mx-auto px-10 ">
+        <div className="">
+          <h3 className=" inline text-4xl border-b-4 border-gray-500">About</h3>
+        </div>
+        <div>
+          <p className="text-xl mt-20 pb-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="text-xl ">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
